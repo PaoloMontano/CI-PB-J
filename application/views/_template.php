fa2333230a74c68a96e20bdf6b5caae46990575f
Brycen <dorsay@live.ca>
2015-01-28 10:28:45 -0800

    Including jquery and css reset

---
+++ application/views/_template.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{title}</title>
+    <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="/assets/css/reset.css" rel="stylesheet" media="screen"/>
+    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
+</head>
+<body>
+    <div class="container">
+        <!-- Header -->
+        <div class="row">
+            {header}
+        </div>
+
+        <!-- Content -->
+        <div class="row">
+            {content}
+        </div>
+
+        <!-- Footer -->
+        <div class="row">
+            {footer}
+        </div>
+
+    </div>
+
+    <script type="javascript" src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script type="javascript" src="/assets/vendor/jquery/jquery-2.1.3.min.js"></script>
+</body>
+</html>
