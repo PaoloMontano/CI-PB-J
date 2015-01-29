0054c5e34bd242658c6d2f6fc381f0185a7e6859
Brycen <dorsay@live.ca>
2015-01-29 14:24:41 -0800

    Eat, Play, and Sleep controllers and views

---
+++ application/views/eat.php
@@ -0,0 +1,20 @@
+<div id="searchnav">
+    <div id="searchnav-contents">
+        <table>
+            <tr>
+                <td id="search-rating">
+                    Rating: <img src="/assets/images/stars.png" alt="rating" width="100px"/>
+                </td>
+                <td id="search-value">
+                    Value: <img src="/assets/images/stars.png" alt="value" width="100px"/>
+                </td>
+                <td id="search-bar">
+                    <input type="text" placeholder="Keyword Search" size="30"/>
+                </td>
+                <td id="search-button">
+                    <button type="button">Search</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
