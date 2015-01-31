4552fb6f8d3845caea1f3a2a1efe0fe87a3359e7
Brycen Dorsay <dorsay@live.ca>
2015-01-31 02:31:18 -0800

    Eat page content

--- application/views/eat.php
+++ application/views/eat.php
@@ -1,27 +1,154 @@
-<div id="searchnav">
-    <div class="navbar navbar-default">
-        <div class="container">
-            <form class="form-inline" role="form" action="">
-                <div class="form-group">
-                    <label for="searchnav-rating">Rating:</label>
-                    <div>*****</div>
-                </div>
-                <div class="form-group">
-                    <label for="searchnav-value">Value:</label>
-                    <div>*****</div>
-                </div>
-                <div class="form-group">
-                    <label for="searchnav-searchbox">Search:</label>
-                    <input class="form-control" id="searchnav-searchbox" type="text"/>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </div>
-            </form>
-        </div>
+<div id="searchnav" class="navbar navbar-default">
+    <div class="container">
+        <form class="form-inline" role="form" action="">
+            <div class="form-group">
+                <label for="searchnav-rating">Rating:</label>
+                <div>*****</div>
+            </div>
+            <div class="form-group">
+                <label for="searchnav-value">Value:</label>
+                <div>*****</div>
+            </div>
+            <div class="form-group">
+                <label for="searchnav-searchbox">Search:</label>
+                <input class="form-control" id="searchnav-searchbox" type="text"/>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-default">Submit</button>
+            </div>
+        </form>
     </div>
 </div>
 
 <div class="container" id="content">
-    content for the page
+    <div id="eat">
+        <table class="results">
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_tuc.jpeg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>Tuc Craft Kitchen</h3>
+                    Tuc Craft Kitchen is a restaurant and bar in Gastown, Vancouver.
+                    Our vision was to create a gathering place that feels like it has always been here.
+                    <br/>Our food is elegant, rustic, honest... <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_pig.jpg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>The Flying Pig</h3>
+                    I know what you’re thinking. The Flying Pig? You must have lots of pork on the menu,
+                    right? Nothing could be further from the truth. To be honest, our menu breakdown has
+                    a wide range of items and, yes, a small... <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_guu.jpg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>Guu with Garlic</h3>
+                    Guu ​with Garlic is all about “Garlic is Sexy”. Also called “Guu Robson”, this is the
+                    second Guu to open. Sexy is not just about the outside, but it’s the fundamental
+                    attitude reflecting energy. <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_tuc.jpeg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>Tuc Craft Kitchen</h3>
+                    Tuc Craft Kitchen is a restaurant and bar in Gastown, Vancouver.
+                    Our vision was to create a gathering place that feels like it has always been here.
+                    <br/>Our food is elegant, rustic, honest... <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_pig.jpg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>The Flying Pig</h3>
+                    I know what you’re thinking. The Flying Pig? You must have lots of pork on the menu,
+                    right? Nothing could be further from the truth. To be honest, our menu breakdown has
+                    a wide range of items and, yes, a small... <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_guu.jpg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>Guu with Garlic</h3>
+                    Guu ​with Garlic is all about “Garlic is Sexy”. Also called “Guu Robson”, this is the
+                    second Guu to open. Sexy is not just about the outside, but it’s the fundamental
+                    attitude reflecting energy. <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_tuc.jpeg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>Tuc Craft Kitchen</h3>
+                    Tuc Craft Kitchen is a restaurant and bar in Gastown, Vancouver.
+                    Our vision was to create a gathering place that feels like it has always been here.
+                    <br/>Our food is elegant, rustic, honest... <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_pig.jpg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>The Flying Pig</h3>
+                    I know what you’re thinking. The Flying Pig? You must have lots of pork on the menu,
+                    right? Nothing could be further from the truth. To be honest, our menu breakdown has
+                    a wide range of items and, yes, a small... <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+            <tr>
+                <td class="results-image"><img src="/public/images/eat_guu.jpg" alt="pic" width="100" height="100"></td>
+                <td class="results-description">
+                    <h3>Guu with Garlic</h3>
+                    Guu ​with Garlic is all about “Garlic is Sexy”. Also called “Guu Robson”, this is the
+                    second Guu to open. Sexy is not just about the outside, but it’s the fundamental
+                    attitude reflecting energy. <a href="#">Learn more.</a>
+                </td>
+                <td class="results-rating">
+                    Rating: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                    <br/>
+                    Value: <img src="/public/images/stars.png" alt="stars" width="100px"/>
+                </td>
+            </tr>
+        </table>
+    </div>
 </div>
