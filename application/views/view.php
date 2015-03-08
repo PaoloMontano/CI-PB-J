d8f6ead762dcee97395df833a2b8477a9059e409
Justin Chee <jchee12@my.bcit.ca>
2015-03-07 17:57:19 -0800

    Made view mobile friendly

--- application/views/view.php
+++ application/views/view.php
@@ -1,21 +1,20 @@
-<div class="row, container" id="content">
+<div class="container" id="content">
     
     <h1>{title}</h1>
     <a style="padding-left: 10px;" href="{link}">{link}</a>
+    <br/><br/>
     
-    <table>
-        <tr>
-            <td style="vertical-align:top;">
-                <img src="/assets/images/{image}" alt="" class="view-image"/>
-            </td>
-            <td style="vertical-align:top;">
-                <b>Phone:</b> {phoneId} <br/>
-                <b>Value:</b> {value} <br/>
-                <b>Rating:</b> {rating} <br/><br/>
-                {desc}
-            </td>
-        </tr>
-    </table>
+    <div class="row">
+        <div class="col-md-4 col-xs-12">
+            <img src="/assets/images/{image}" alt="" class="view-image"/>
+        </div>
+        <div class="col-md-8 col-xs-12">
+            <b>Phone:</b> {phoneId} <br/>
+            <b>Value:</b> {value} <br/>
+            <b>Rating:</b> {rating} <br/><br/>
+            {desc}
+        </div>
+    </div>
     
     <br/>
     <a href="../" role="button" class="btn btn-default">Go Back</a>
@@ -24,25 +23,3 @@
     <br/>
     
 </div>
-
-<!--Mobile friendly test-->
-<!--<div class="row, container form-inline" id="content">
-    <form class="form-inline row text-center" role="form" action="">
-    <h1>{title}</h1>
-    <a style="padding-left: 10px;" href="{link}">{link}</a>
-    <br/>
-    <div class="form-group col-md3">
-        <img src="/assets/images/{image}" alt="" class="view-image"/>
-    </div>
-    <div class="form-group col-md3">
-        Phone: {phoneId} <br/>
-        Value: {value} <br/>
-        Rating: {rating} <br/>
-        {desc}
-    </div>
-    <br/>
-    <a href="../" role="button" class="btn btn-default">Go Back</a>
-    <a href="{link}" role="button" class="btn btn-success">Visit</a>
-    </form>
-    <br/><br/>
-</div>-->
